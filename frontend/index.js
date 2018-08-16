cf6a56354a43a8711b3856677dec611059873949
petrovich <mazahakaforever@ya.ru>
2018-08-16 18:23:08 +0300

    First approach firexproxy

--- frontend/index.js
+++ frontend/index.js
@@ -4,19 +4,21 @@ $(document).ready(function () {
     var portSpan = $('#port');
     var countrySpan = $('#country');
     var pingSpan = $('#ping');
+    var lossSpan = $('#loss');
 
     function updateProxy() {
         $.get('/proxy', null, function (data) {
             currentProxy = data;
-            ipSpan.text(currentProxy.ipAddress);
+            ipSpan.text(currentProxy.server);
             portSpan.text(currentProxy.port);
             countrySpan.text(currentProxy.country);
-            pingSpan.text(currentProxy.ping);
+            pingSpan.text(currentProxy.ping_time_ms);
+            lossSpan.text(Math.floor(currentProxy.loss_ratio * 100));
         });
     }
 
     function toTelegram() {
-        var win = window.open(`tg://socks?server=${currentProxy.ipAddress}&port=${currentProxy.port}`, '_blank');
+        var win = window.open(`tg://socks?server=${currentProxy.server}&port=${currentProxy.port}`, '_blank');
         win.focus();
         setTimeout(() => {win.close();}, 1000);
     }
--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const ProxyServer = require('./ProxyServer.js');
-const SocksProxy = require('./SocksProxy.js');
-const GatherProxy = require('./GatherProxy.js');
-
+const request = require('request');
 const port = process.env.PORT || 8080;
-const updateInterval = 10; // 10 minutes;
 
 let app = express();
 app.use(cors());
@@ -14,12 +10,9 @@ app.listen(port, () => {
     console.log(`Started server on ${port}`);
 });
 
-let proximator = new ProxyServer(GatherProxy, updateInterval);
-
 app.get('/proxy', (req, res) => {
-    let proxy = proximator.getProxy();
-    console.dir(proxy);
-    if (!proxy) return res.status(500).json({error: 'No proxies available'});
-
-    return res.json(proxy);
+    request(`http://firexproxy.com:4040/v1/proxy?protocol=SOCKS5&limit=1&offset=${Math.round(Math.random()*40)}`, (err, response, body) => {
+            if (err) return res.status(500).json({error: err});
+            return res.json(JSON.parse(body)[0]);
+    });
 });
